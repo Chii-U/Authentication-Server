706e541e155e366f9f3872317967d549ef5b4f70
SURI <snyeg28842@gmail.com>
2024-04-30 00:19:06 +0900

    feat: 필터 생성하기! 기본 코드

---
+++ src/main/java/com/example/authenticationserver/JWT/JwtAuthenticationFilter.java
@@ -0,0 +1,20 @@
+package com.example.authenticationserver.JWT;
+
+import com.example.authenticationserver.JWT.JwtTokenProvider;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+//필터 직접 구현
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}
--- src/main/java/com/example/authenticationserver/config/SecurityConfig.java
+++ src/main/java/com/example/authenticationserver/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.authenticationserver.config;
 
 import com.example.authenticationserver.JWT.JwtTokenProvider;
+import com.example.authenticationserver.JWT.JwtAuthenticationFilter;
 import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
