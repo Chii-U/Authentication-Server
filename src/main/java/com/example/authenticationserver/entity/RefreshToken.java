395c5dd64b3dfda725e95bc356cd72a2dcf1fa2b
SURI <snyeg28842@gmail.com>
2024-04-30 15:00:00 +0900

    feat: 리프레시토큰을 jpa를 통해 redis로 접근하는 엔티티, 리포 인터페이스 생성

---
+++ src/main/java/com/example/authenticationserver/entity/RefreshToken.java
@@ -0,0 +1,19 @@
+package com.example.authenticationserver.entity;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Builder@Getter@Setter
+@RedisHash(value = "refresh")
+public class RefreshToken {
+    @Id
+    private String token;
+    private String username;
+    @TimeToLive
+    private long ttl;
+}
---
+++ src/main/java/com/example/authenticationserver/repository/RefreshRepository.java
@@ -0,0 +1,14 @@
+package com.example.authenticationserver.repository;
+
+import com.example.authenticationserver.entity.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository //이거 레디스 접근하는 리포임.
+public interface RefreshRepository extends CrudRepository<RefreshToken,String> {
+    Optional<RefreshToken> findByToken(String token);
+    boolean deleteByToken(String token);
+    boolean existsByToken(String token);
+}
