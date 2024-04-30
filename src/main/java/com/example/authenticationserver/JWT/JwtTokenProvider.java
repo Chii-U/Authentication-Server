1cf5edbd5f49ec8b329cbdc2a91d7943c9bc02cb
SURI <snyeg28842@gmail.com>
2024-04-30 14:08:12 +0900

    fix: 액세스토큰 생성 구현

--- src/main/java/com/example/authenticationserver/JWT/JwtTokenProvider.java
+++ src/main/java/com/example/authenticationserver/JWT/JwtTokenProvider.java
@@ -1,6 +1,9 @@
 package com.example.authenticationserver.JWT;
 
 import com.example.authenticationserver.entity.User;
+import com.example.authenticationserver.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -12,6 +15,8 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+
+import java.util.Date;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -21,6 +26,9 @@ public class JwtTokenProvider {
     @Value("${paint.domain.name}")
     private String domain;
     private final Key key;
+
+    @Autowired
+    UserRepository userRepository;
 
     public JwtTokenProvider(@Value("${PAINT_JWT_KEY}") String secretKey) {
         byte[] secret = Decoders.BASE64.decode(secretKey);
@@ -32,12 +40,24 @@ public class JwtTokenProvider {
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.joining(",")); // ,로
 
-        User user; //여기에 autentication의 이름과 리포의 이름을 대조하는 코드 필요, 맞으면 해당 객체를 불러옴
+        //활용 가능한 정보중에 해당 정보가 있는지 (이때 비밀번호는 필터에서 인증되므로 유저네임이랑, 사용할수있는지만 대조하면 된다.)
+        User user = userRepository.findByUsernameAndIsEnabledTrue(authentication.getName()).orElseThrow(IllegalArgumentException::new); //여기에 autentication의 이름과 리포의 이름을 대조하는 코드 필요, 맞으면 해당 객체를 불러옴
         //실제 액세스토큰 생성, 일단 이메일 or username, 클레임, 역할정도 넣으면 될 듯
         return doGenerateAccessToken(user.getUsername(), Map.of("username",user.getUsername(), "https://chi-iu.com/claims/what-role",authorities));
     }
 
-    private String doGenerateAccessToken(String username, Map<String, String> username1) {
+    private String doGenerateAccessToken(String username, Map<String, Object> claims) {
+        long now = (new Date()).getTime();
+        Date acessTokenExpiresIn = new Date(now + 240000); // 4분정도... 60초 * 4분 * 1000ms
+        return Jwts.builder()
+                .setHeaderParam("type","jwt") // 많이들 씀, jwt라는거 알려는 줘야..ㅎ?
+                .setIssuer(domain)
+                .setIssuedAt(new Date())
+                .setClaims(claims)
+                .setSubject(username)
+                .setExpiration(acessTokenExpiresIn) //이때 만료
+                .signWith(key, SignatureAlgorithm.HS256) // 그냥 많이들 사용함
+                .compact();
     }
 
 }
