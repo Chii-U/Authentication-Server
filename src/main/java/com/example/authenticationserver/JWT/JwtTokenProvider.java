aa5a22fa93e49b73a5dd67dbd397543c043512da
SURI <snyeg28842@gmail.com>
2024-04-30 15:56:39 +0900

    fix: 리프레시 토큰 검증 구현, 실제 검증 코드 수정 후 오버로딩

--- src/main/java/com/example/authenticationserver/JWT/JwtTokenProvider.java
+++ src/main/java/com/example/authenticationserver/JWT/JwtTokenProvider.java
@@ -1,15 +1,17 @@
 package com.example.authenticationserver.JWT;
 
 import com.example.authenticationserver.entity.Account;
+import com.example.authenticationserver.entity.RefreshToken;
 import com.example.authenticationserver.entity.User;
-import com.example.authenticationserver.repository.UserRepository;
+import com.example.authenticationserver.service.TokenService;
+import com.example.authenticationserver.service.UserService;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
 import io.jsonwebtoken.security.SecurityException;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -20,10 +22,7 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -31,15 +30,19 @@ import java.util.stream.Collectors;
 public class JwtTokenProvider {
     @Value("${paint.domain.name}")
     private String domain;
-    private final Key key;
+    private final Key accessKey;
+    private final Key refreshKey;
 
-    @Autowired
-    UserRepository userRepository;
+
+    UserService userService;
+    TokenService tokenService;
 
     // 생성자~ 일단 가장중요한 키 초기화
-    public JwtTokenProvider(@Value("${PAINT_JWT_KEY}") String secretKey) {
+    public JwtTokenProvider(@Value("${PAINT_JWT_KEY}") String secretKey, @Value("${PAINT_REFRESH_SECRET}") String refreshKey) {
         byte[] secret = Decoders.BASE64.decode(secretKey);
-        this.key = Keys.hmacShaKeyFor(secret);
+        this.accessKey = Keys.hmacShaKeyFor(secret);
+        byte[] secret2 = Decoders.BASE64.decode(refreshKey);
+        this.refreshKey = Keys.hmacShaKeyFor(secret2);
     }
 
     //login할때
@@ -49,7 +52,7 @@ public class JwtTokenProvider {
                 .collect(Collectors.joining(",")); // ,로
 
         //활용 가능한 정보중에 해당 정보가 있는지 (이때 비밀번호는 필터에서 인증되므로 유저네임이랑, 사용할수있는지만 대조하면 된다.)
-        User user = userRepository.findByUsernameAndIsEnabledTrue(authentication.getName()).orElseThrow(IllegalArgumentException::new); //여기에 autentication의 이름과 리포의 이름을 대조하는 코드 필요, 맞으면 해당 객체를 불러옴
+        User user = userService.findByUsernameAndIsEnabledTrue(authentication.getName()); //여기에 autentication의 이름과 리포의 이름을 대조하는 코드 필요, 맞으면 해당 객체를 불러옴
         //실제 액세스토큰 생성, 일단 이메일 or username, 클레임, 역할정도 넣으면 될 듯
         return doGenerateAccessToken(user.getUsername(), Map.of("username",user.getUsername(), "https://chi-iu.com/claims/what-role",authorities));
     }
@@ -66,12 +69,16 @@ public class JwtTokenProvider {
                 .setClaims(claims)
                 .setSubject(username)
                 .setExpiration(acessTokenExpiresIn) //이때 만료
-                .signWith(key, SignatureAlgorithm.HS256) // 그냥 많이들 사용함
+                .signWith(accessKey, SignatureAlgorithm.HS256) // 그냥 많이들 사용함
                 .compact();
     }
 
     // 들어온 액세스토큰의 시간 값을 포함한 등등이 유효한지
     public boolean validateAccessToken(String token) {
+        return validateAccessToken(token,accessKey);
+    }
+    // 리프레시 검증하는 경우때문에 오버로딩함.
+    private boolean validateAccessToken(String token,Key key) {
         try {
             Jwts.parserBuilder()
                     .setSigningKey(key)
@@ -108,7 +115,7 @@ public class JwtTokenProvider {
     private Claims parseClaims(String token) {
         try {
             return Jwts.parserBuilder()
-                    .setSigningKey(key)
+                    .setSigningKey(accessKey)
                     .build()
                     .parseClaimsJws(token)
                     .getBody();
@@ -119,11 +126,24 @@ public class JwtTokenProvider {
 
     // 들어온 리프레시토큰이 액세스토큰과 비교했을때 같은 사람껀지, 유효한지.
     public boolean validateRefreshToken(String refreshToken, String token) {
+        try {
+            RefreshToken tokenEntity = tokenService.findByToken(refreshToken);
+            String username = getAuthentication(token).getName();
+            User user = userService.findByUsernameAndIsEnabledTrue(username);
 
+            if (user.getUsername().equals(tokenEntity.getUsername())) {
+                System.out.println("ok 맞음, 레디스에 있던 아이디랑, 니가 찾는 db에 있는 유저 아이디 일치함");
+                return validateAccessToken(tokenEntity.getToken(),refreshKey);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
     }
 
     // 액세스토큰을 가지고 인증정보를 빼서 유효기간을 늘리기
     public String reGenerateAccessToken(String token) {
+
     }
 
     // rtr 방식을 위해 들어온 리프레시토큰을 대체하기, 특히 Ttl값은 이전 토큰을 파싱하여 가져다 쓴다.
