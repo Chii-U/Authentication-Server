fa8d35873ea837944c38bdce284260ae29573e92
SURI <snyeg28842@gmail.com>
2024-04-30 16:53:42 +0900

    feat: database 컨피그레이션 파일

--- src/main/java/com/example/authenticationserver/AuthenticationServerApplication.java
+++ src/main/java/com/example/authenticationserver/AuthenticationServerApplication.java
@@ -2,8 +2,10 @@ package com.example.authenticationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.example.authenticationserver.repository")
 public class AuthenticationServerApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/authenticationserver/config/RedisConfig.java
@@ -0,0 +1,58 @@
+package com.example.authenticationserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.convert.KeyspaceConfiguration;
+import org.springframework.data.redis.core.convert.MappingConfiguration;
+import org.springframework.data.redis.core.index.IndexConfiguration;
+import org.springframework.data.redis.core.index.IndexDefinition;
+import org.springframework.data.redis.core.index.SimpleIndexDefinition;
+import org.springframework.data.redis.core.mapping.RedisMappingContext;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.util.Collections;
+import java.util.Objects;
+
+@Configuration
+@EnableRedisRepositories(indexConfiguration = RedisConfig.PaintIndexConfig.class)
+public class RedisConfig {
+
+    @Autowired
+    Environment env;
+
+    private final int port = Integer.parseInt(Objects.requireNonNull(env.getProperty("spring.data.redis.port")));
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(Objects.requireNonNull(env.getProperty("spring.data.redis.host")),port);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    @Bean public RedisMappingContext keyValueMappingContext() {
+        return new RedisMappingContext( new MappingConfiguration(new PaintIndexConfig(), new KeyspaceConfiguration()));
+    }
+    public static class PaintIndexConfig extends IndexConfiguration {
+        @Override
+        protected Iterable<IndexDefinition> initialConfiguration() {
+            return Collections.singleton(new SimpleIndexDefinition("refresh","token"));
+        }
+    }
+}
