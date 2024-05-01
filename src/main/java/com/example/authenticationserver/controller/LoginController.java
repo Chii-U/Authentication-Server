82f5dd84065c4d70532877035ea9999a20af80ee
SURI <snyeg28842@gmail.com>
2024-05-02 01:14:41 +0900

    fix: user라는 경로가 내부 중복이라 users로 변경

--- src/main/java/com/example/authenticationserver/controller/LoginController.java
+++ src/main/java/com/example/authenticationserver/controller/LoginController.java
@@ -4,27 +4,44 @@ import com.example.authenticationserver.dto.IDPWDto;
 import com.example.authenticationserver.dto.JwtToken;
 import com.example.authenticationserver.global.BaseException;
 import com.example.authenticationserver.global.BaseResponse;
+import com.example.authenticationserver.global.BaseResponseStatus;
+import com.example.authenticationserver.service.LoginService;
 import com.example.authenticationserver.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import static com.example.authenticationserver.global.BaseResponseStatus.SUCCESS;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users") // 인그레스에서 기본경로에 user가 있어서 매핑이 제대로 안되기에, 경로 이름을 바꿈
 public class LoginController {
     @Autowired
     UserService userService;
+
+    @Autowired
+    LoginService loginService;
 
     @PostMapping("/login")
     public BaseResponse<JwtToken> login(
             @RequestBody IDPWDto idpwDto, HttpServletResponse response
     ) throws BaseException {
-        JwtToken token = userService.login(idpwDto,response);
-        return new BaseResponse<JwtToken>(token);
+        JwtToken token = loginService.login(idpwDto,response);
+        return new BaseResponse<>(token);
+    }
+
+    @PostMapping("/logout")
+    public BaseResponse<Void> logout(HttpServletRequest request, HttpServletResponse response) throws BaseException {
+        userService.logout(request,response);
+        return new BaseResponse<>(SUCCESS);
+    }
+    @GetMapping("/test")
+    public BaseResponse<Void> test(){
+        userService.test();
+        return new BaseResponse<Void>(SUCCESS);
     }
 }
