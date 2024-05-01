c53548eeba99d8bbc34a551c22243341acc670aa
SURI <snyeg28842@gmail.com>
2024-05-02 01:16:35 +0900

    fix: 어카운트 만들었으나... security core의 User와 뭐 형식이 더 추가될 게 없어서 그냥 이름은 겹쳐서 헷갈리지만 User를 사용하기로 함

--- src/main/java/com/example/authenticationserver/entity/Account.java
+++ src/main/java/com/example/authenticationserver/entity/Account.java
@@ -1,29 +1,28 @@
 package com.example.authenticationserver.entity;
 
-
-import com.example.authenticationserver.enumerated.Authority;
-import lombok.Getter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-
-// 데이터베이스에 접근하는 엔티티는 아닙니다.
-// db 접근을 줄이고자 Jwt를 사용할 때 필요한 적당한 정보만 저장된 객체!
-@Getter
-public class Account implements UserDetails {
-    private String username;
-    private String password;
-    private Collection<GrantedAuthority> authorities;
-    private boolean isEnabled;
-    private boolean isAccountNonLocked;
-    private boolean isAccountNonExpired;
-    private boolean isCredentialsNonExpired;
-
-    public Account(String username, String password, Collection<GrantedAuthority> authorities) {
-        this.username = username;
-        this.password = password;
-        this.authorities = authorities;
-    }
-
-}
+//
+//import com.example.authenticationserver.enumerated.Authority;
+//import lombok.Getter;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Collection;
+//
+//// 데이터베이스에 접근하는 엔티티는 아닙니다.
+//// db 접근을 줄이고자 Jwt를 사용할 때 필요한 적당한 정보만 저장된 객체!
+//@Getter
+//public class Account extends User {
+//    private String username;
+//    private String password;
+//    private Collection<GrantedAuthority> authorities;
+//    private boolean isEnabled;
+//    private boolean isAccountNonLocked;
+//    private boolean isAccountNonExpired;
+//    private boolean isCredentialsNonExpired;
+//
+//    public Account(String username, String password, Collection<GrantedAuthority> authorities) {
+//        super(username,password,authorities);
+//    }
+//
+//}
