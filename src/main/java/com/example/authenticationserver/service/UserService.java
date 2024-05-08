b498dba0d9d8e165a3240c5e2b97359325824645
SURI <snyeg28842@gmail.com>
2024-05-08 20:57:08 +0900

    fix: 새로운 예외 추가, save 함수 관련 구현

--- src/main/java/com/example/authenticationserver/service/UserService.java
+++ src/main/java/com/example/authenticationserver/service/UserService.java
@@ -1,39 +1,22 @@
 package com.example.authenticationserver.service;
 
-import com.example.authenticationserver.JWT.JwtTokenProvider;
-import com.example.authenticationserver.dto.IDPWDto;
-import com.example.authenticationserver.dto.JwtToken;
 import com.example.authenticationserver.dto.SignUpDTO;
 import com.example.authenticationserver.entity.User;
 import com.example.authenticationserver.enumerated.Authority;
 import com.example.authenticationserver.global.BaseException;
-import com.example.authenticationserver.global.BaseResponse;
-import com.example.authenticationserver.global.BaseResponseStatus;
 import com.example.authenticationserver.repository.UserRepository;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.UUID;
 
 import static com.example.authenticationserver.global.BaseResponseStatus.*;
 
