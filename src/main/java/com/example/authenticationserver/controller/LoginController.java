0a3edb4eba96530b964ca200106495574ae33134
SURI <snyeg28842@gmail.com>
2024-05-09 22:28:14 +0900

    fix: 사인업 드롭아웃 구현

--- src/main/java/com/example/authenticationserver/controller/LoginController.java
+++ src/main/java/com/example/authenticationserver/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.example.authenticationserver.controller;
 import com.example.authenticationserver.dto.IDPWDto;
 import com.example.authenticationserver.dto.JwtToken;
 import com.example.authenticationserver.dto.SignUpDTO;
+import com.example.authenticationserver.entity.User;
 import com.example.authenticationserver.global.BaseException;
 import com.example.authenticationserver.global.BaseResponse;
 import com.example.authenticationserver.service.LoginService;
@@ -11,8 +12,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+import static com.example.authenticationserver.global.BaseResponseStatus.PASSWORD_NOT_MATCH;
 import static com.example.authenticationserver.global.BaseResponseStatus.SUCCESS;
 
 @RestController
@@ -24,6 +30,7 @@ public class LoginController {
 
     @Autowired
     LoginService loginService;
+
 
     @PostMapping("/login")
     public BaseResponse<JwtToken> login(
@@ -45,6 +52,25 @@ public class LoginController {
     ) throws BaseException {
         userService.signup(signUpDTO);
         return new BaseResponse<>(SUCCESS);
+    }
+
+    @PostMapping("/dropout")
+    public BaseResponse<Void> dropout(
+            @AuthenticationPrincipal User user,
+            @RequestBody Map<String,String> data
+    ) throws BaseException {
+        if(!loginService.matchPassword(data.get("password"), user.getPassword())){
+            throw new BaseException(PASSWORD_NOT_MATCH);
+        }
+        userService.dropout(user.getUsername());
+        return new BaseResponse<Void>(SUCCESS);
+    }
+
+    // 취약해 보인다.
+    @PostMapping("id-dup")
+    public BaseResponse<Void> idDupCheck(
+    ){
+        return new BaseResponse<Void>(SUCCESS);
     }
 
     @GetMapping("/test")
