66c61e445639da45c91bee75e3dd33243a6b1ae9
SURI <snyeg28842@gmail.com>
2024-05-09 22:29:59 +0900

    fix: 업데이트 구문을 쿼리로 날리고, 이로써 소프트 딜리트 구현 가능

--- src/main/java/com/example/authenticationserver/repository/UserRepository.java
+++ src/main/java/com/example/authenticationserver/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.authenticationserver.repository;
 
 import com.example.authenticationserver.entity.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,4 +14,13 @@ public interface UserRepository extends MongoRepository<User,String> {
     Optional<User> findByUsernameAndIsEnabledTrue(String username);
 
     boolean existsByEmail(String email);
+
+    @Query("{'username' : ?0}")
+    @Update("{'$set': {'is_enabled': false}}")
+    void disableByUsername(String username);
+
+    @Query("{'username' : ?0}")
+    @Update("{'$set': {'is_account_nonlocked': false}}")
+    void lockByUsername(String username);
+
 }
