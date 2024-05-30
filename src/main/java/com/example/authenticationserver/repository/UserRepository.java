fff3ef4debf7a2ea2a81af105d95cc19d89a3237
SURI <snyeg28842@gmail.com>
2024-05-30 23:50:11 +0900

    fix: 이메일이 인증되었을 때, 유저가 탈퇴했을 때 사용할 기능

--- src/main/java/com/example/authenticationserver/repository/UserRepository.java
+++ src/main/java/com/example/authenticationserver/repository/UserRepository.java
@@ -7,13 +7,18 @@ import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.Update;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends MongoRepository<User,String> {
     Optional<User> findByUsernameAndIsEnabledTrue(String username);
 
+    boolean existsByUsername(String username);
+
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 
     @Query("{'username' : ?0}")
     @Update("{'$set': {'is_enabled': false}}")
@@ -23,4 +28,16 @@ public interface UserRepository extends MongoRepository<User,String> {
     @Update("{'$set': {'is_account_nonlocked': false}}")
     void lockByUsername(String username);
 
+    @Query("{'email' : ?0}")
+    @Update("{'$set': {'is_enabled': ?1}}")
+    void updateEnable(String email, boolean enable);
+
+    @Query("{'username' :  ?0}")
+    @Update("{'$set': {'drop_date': ?1}}")
+    void updateDrop(String username, LocalDateTime dropDate);
+
+    @Query("db.user.find({\n" +
+            "    drop_date: { $lte: new Date(new Date() - 90 * 24 * 60 * 60 * 1000) }\n" +
+            "})")
+    List<User> findAllByOldDrop();
 }
