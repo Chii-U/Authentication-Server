137425a6df6da6474dbf1f57fef699105c5a6a57
SURI <snyeg28842@gmail.com>
2024-05-30 23:42:35 +0900

    fix: 인증 활성화

--- src/main/java/com/example/authenticationserver/AuthenticationServerApplication.java
+++ src/main/java/com/example/authenticationserver/AuthenticationServerApplication.java
@@ -2,9 +2,13 @@ package com.example.authenticationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableFeignClients
 @EnableMongoRepositories(basePackages = "com.example.authenticationserver.repository")
 public class AuthenticationServerApplication {
 
