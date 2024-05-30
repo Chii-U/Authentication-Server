5cca74801d7fbaac2e21dd68c62fbf7086531609
SURI <snyeg28842@gmail.com>
2024-05-30 23:47:19 +0900

    fix: 아이디 어노테이션 문제로 추가

--- src/main/java/com/example/authenticationserver/entity/RefreshToken.java
+++ src/main/java/com/example/authenticationserver/entity/RefreshToken.java
@@ -7,11 +7,14 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
 
 @Builder@Getter@Setter
 @RedisHash(value = "refresh")
 public class RefreshToken {
     @Id
+    private String refreshId;
+    @Indexed
     private String token;
     private String username;
     @TimeToLive
