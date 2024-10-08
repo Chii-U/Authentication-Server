3ddcbd439fa257aba29082fdab01cd04627c4ff8
haeun161 <80445078+haeun161@users.noreply.github.com>
2024-10-08 23:33:35 +0900

    Create DiseaseRequestDto.java

---
+++ src/main/java/com/example/authenticationserver/dto/DiseaseRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.authenticationserver.dto;
+
+import lombok.*; 
+
+@Getter@Setter@AllArgsConstructor@RequiredArgsConstructor@Data
+public class DiseaseRequestDto {
+    String username;
+    String disease;
+}
