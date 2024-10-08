d4587b80507cd4b213f4ddf243113bd987ab5989
haeun161 <80445078+haeun161@users.noreply.github.com>
2024-10-08 23:30:35 +0900

    Create DiseaseClient.java

---
+++ src/main/java/com/example/authenticationserver/client/DiseaseClient.java
@@ -0,0 +1,23 @@
+package com.example.authenticationserver.client;
+
+import com.example.authenticationserver.config.MultiPartConfig;
+import com.example.authenticationserver.dto.DiseaseRequestDto;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@FeignClient(name = "disease", url = "${spring.cloud.openfeign.client.disease-url}", configuration = MultiPartConfig.class)
+@Qualifier("disease")
+public interface DiseaseClient {
+
+    @PostMapping(value = "/disease", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
+    Object predictDisease(
+            @RequestBody DiseaseRequestDto req);
+
+    // GET 메서드 추가
+    @GetMapping(value = "/disease", produces = APPLICATION_JSON_VALUE)
+    Object getDisease(
+            @RequestParam("username") String username);
+}
