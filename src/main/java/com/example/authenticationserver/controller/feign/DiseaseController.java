de9425d6d8d458691ebd9bf2b4735b3c16568a7d
haeun161 <80445078+haeun161@users.noreply.github.com>
2024-10-08 23:52:34 +0900

    fix: DiseaseController get,post 수정

--- src/main/java/com/example/authenticationserver/controller/feign/DiseaseController.java
+++ src/main/java/com/example/authenticationserver/controller/feign/DiseaseController.java
@@ -1,6 +1,7 @@
 package com.example.authenticationserver.controller.feign;
 
 import com.example.authenticationserver.client.DiseaseClient;
+import com.example.authenticationserver.dto.DiseaseRequestDto;
 import com.example.authenticationserver.entity.Account;
 import com.example.authenticationserver.global.BaseException;
 import com.example.authenticationserver.global.BaseResponse;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,23 @@ public class DiseaseController {
 
     @Autowired
     private DiseaseClient diseaseClient;
+    
+    @PostMapping("/disease")
+    public BaseResponse<Object> predictDisease(
+            @AuthenticationPrincipal Account account,
+            DiseaseRequestDto dto // DTO로 변경하여 요청 본문에서 사용자 이름과 질병 정보를 받음
+    ) throws BaseException {
+        if (account == null) {
+            throw new BaseException(LOGIN_FIRST);
+        }
+
+        dto.setUsername(account.getUsername()); // 인증된 사용자 이름 설정
+
+        // 외부 API 호출
+        Object response = diseaseClient.predictDisease(dto);
+
+        return new BaseResponse<>(response);
+    }
 
     @GetMapping("/disease")
     public BaseResponse<Object> getDisease(
